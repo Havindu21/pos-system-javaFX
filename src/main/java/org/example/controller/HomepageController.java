7d5a3d7c9cbc457d0012425f262640363794b3eb
Havindu21 <havindufonseka@gmail.com>
2023-10-17 12:28:46 +0530

    finishing 4 interfaces

--- src/main/java/org/example/controller/HomepageController.java
+++ src/main/java/org/example/controller/HomepageController.java
@@ -1,15 +1,22 @@
 package org.example.controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.chart.PieChart;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class HomepageController {
+public class HomepageController implements Initializable {
+    public PieChart pieChartHomepage;
     private Parent parent;
     private Stage stage;
     private Scene scene;
@@ -21,6 +28,16 @@ public class HomepageController {
         stage.setScene(scene);
         stage.show();
     }
-    
 
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        ObservableList<PieChart.Data> pieObservableList= FXCollections.observableArrayList(
+                new PieChart.Data("Gents",10),
+                new PieChart.Data("Ladies",40),
+                new PieChart.Data("Kids",30),
+                new PieChart.Data("Others",20)
+        );
+        pieChartHomepage.setData(pieObservableList);
+    }
 }
